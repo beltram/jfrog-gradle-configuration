9ac621615062e3ce62f7a496dfdd1270b7bb74fb
beltram.maldant <beltram.maldant@soprabanking.com>
2018-07-09 15:35:51 +0200

    initial commit

---
+++ build.gradle.kts
@@ -0,0 +1,62 @@
+import groovy.lang.Closure
+import groovy.lang.GroovyObject
+import org.gradle.kotlin.dsl.*
+import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
+import org.jetbrains.kotlin.js.translate.context.Namer.kotlin
+import org.jfrog.gradle.plugin.artifactory.dsl.ResolverConfig
+import org.jfrog.gradle.plugin.artifactory.task.ArtifactoryTask
+import org.owasp.dependencycheck.gradle.extension.DependencyCheckExtension
+
+val springBootVersion = "2.0.3.RELEASE"
+
+plugins {
+    val kotlinVersion = "1.2.51"
+    kotlin("jvm") version kotlinVersion
+    kotlin("plugin.spring") version kotlinVersion
+    id("io.spring.dependency-management") version "1.0.5.RELEASE"
+    id("com.jfrog.artifactory") version "4.7.3"
+    id("org.owasp.dependencycheck") version "3.2.1"
+}
+
+subprojects {
+    group = "com.beltram"
+    version = "0.0.1-SNAPSHOT"
+
+    dependencyManagement {
+        imports {
+            mavenBom("org.springframework.boot:spring-boot-starter-parent:$springBootVersion")
+        }
+    }
+
+    dependencies {
+        implementation(kotlin("stdlib-jdk8"))
+        implementation(kotlin("reflect"))
+        compile("org.springframework.boot:spring-boot-starter")
+        testCompile("org.springframework.boot:spring-boot-starter-test")
+    }
+
+    artifactory {
+        fun artifactoryRepo(repoKey: String) = delegateClosureOf<GroovyObject> {
+            setProperty("repoKey", repoKey)
+            setProperty("username", project.findProperty("artifactory_user"))
+            setProperty("password", project.findProperty("artifactory_password"))
+            setProperty("maven", true)
+        }
+        setContextUrl(project.findProperty("artifactory_contextUrl"))
+        resolve(delegateClosureOf<ResolverConfig> {
+            repository(artifactoryRepo("gradle-repo"))
+        })
+    }
+
+    tasks {
+        val build by tasks
+        withType<KotlinCompile> {
+            kotlinOptions {
+                jvmTarget = "1.8"
+                freeCompilerArgs = listOf("-Xjvm-default=compatibility")
+            }
+        }
+        withType<Test> { useJUnitPlatform() }
+        withType<ArtifactoryTask> { dependsOn(build) }
+    }
+}
---
+++ src/main/kotlin/com/beltram/jfroggradleconfiguration/JfrogGradleConfigurationApplication.kt
@@ -0,0 +1,11 @@
+package com.beltram.jfroggradleconfiguration
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class JfrogGradleConfigurationApplication
+
+fun main(args: Array<String>) {
+    runApplication<JfrogGradleConfigurationApplication>(*args)
+}
